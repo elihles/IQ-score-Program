75f8abab1ca4a4f91b0a0c21cd60249f90a5a770
s227398599 <s227398599@LAB1-11>
2023-03-29 15:30:36 +0200

    Add project files.

---
+++ P3T1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.Write("What is your IQ score:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if ((num1 < 0 || num1 > 200))
+                Console.WriteLine("invalid IQ value");
+            else
+            {
+                if (num1 < 100)
+                    Console.WriteLine("IQ is below average");
+                else
+                {
+                    if (num1 == 100)
+                        Console.WriteLine(" IQ is average");
+                    else
+                    {
+                        if (num1 > 100)
+                            Console.WriteLine("IQ is Above average");
+
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
